97cf33a5aebdbb9e8a1187bd84d665a064031703
forever-sumit <forever.sumitsharma@gmail.com>
2018-02-15 19:43:31 +0530

    spec file remove with admin namespace and new file added without admin namespace

--- spec/models/movie_thumbnail_spec.rb
+++ spec/models/movie_thumbnail_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Admin::RecurringPlan, type: :model do
+RSpec.describe MovieThumbnail, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
