c61daa5f7420c26c28e669a7fa25d2977be77fc3
forever-sumit <forever.sumitsharma@gmail.com>
2018-02-28 13:02:06 +0530

    mailing configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'miniflix.tv'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  #mailing configuration
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                  587,
+    user_name:             ENV['Mailer_Email'],
+    password:              ENV['Mailer_Password'],
+    authentication:        'login',
+    domain:                'miniflix.tv',
+    enable_starttls_auto: true
+  }
 end
