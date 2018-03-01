c01f390c85ce80f59938a359e36dc5146d02b19d
forever-sumit <forever.sumitsharma@gmail.com>
2018-03-01 17:40:59 +0530

    genre list api without pagination changes

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::GenresController < ApplicationController
 
   def index
-    genres = Genre.joins(:movies).distinct.paginate(page: params[:page])
-    render json: genres, serializer: Api::V1::CustomGenreSerializer
+    if params[:paginate]
+      genres = Genre.joins(:movies).distinct.paginate(page: params[:page])
+      render json: genres, serializer: Api::V1::CustomGenreSerializer
+    else
+      genres = Genre.joins(:movies).distinct
+      render json: genres
+    end
   end
 end
