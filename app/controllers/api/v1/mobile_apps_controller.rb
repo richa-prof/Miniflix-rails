53da37fb994de96e7d045ad0d4f26971cd6c9205
forever-sumit <forever.sumitsharma@gmail.com>
2018-03-16 17:12:47 +0530

    changes in twillo api response and parameter

--- app/controllers/api/v1/mobile_apps_controller.rb
+++ app/controllers/api/v1/mobile_apps_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::MobileAppsController < Api::V1::ApplicationController
 
   def share_app_link
-    response = TwilioService.new(params[:phone_number]).call()
+    message = "Download the app now to watch unlimited short films! #{ENV['smart_url']}"
+    response = TwilioService.new(params[:phone_number], message).call()
+    response[:message] = "Download link successfully sent to your phone number" if response[:success]
     render json: response
   end
 end
--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -1,22 +1,24 @@
 class TwilioService
 
-  def initialize(phone_number)
+  def initialize(phone_number, message)
     @phone_number = phone_number
+    @message = message
   end
 
   def call
-    send_twilio_message(@phone_number)
+    send_twilio_message(@phone_number, @message)
   end
 
   private
-    def send_twilio_message(phone_number)
+    def send_twilio_message(phone_number, message)
       begin
         TWILIO.api.account.messages.create(
+
           from: ENV['TWILIO_PHONE_NUMBER'],
           to: phone_number,
-          body: "Download the app now to watch unlimited short films! #{ENV['smart_url']}"
+          body: message
         )
-        { success: true, message: 'Download link send successfully.' }
+        { success: true}
       rescue Twilio::REST::RestError => error
         { success: false, message: error.message }
       end
