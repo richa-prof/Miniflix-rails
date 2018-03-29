4e7ac674db95e0be9851581b11189071ab4f6c94
forever-sumit <forever.sumitsharma@gmail.com>
2018-03-29 12:59:22 +0530

    movie trailer field added

--- app/serializers/api/v1/movie_serializer.rb
+++ app/serializers/api/v1/movie_serializer.rb
@@ -1,14 +1,12 @@
 class Api::V1::MovieSerializer < ActiveModel::Serializer
-  attributes :id, :slug, :name, :title, :description, :festival_laureates, :directed_by, :language, :released_date, :video_duration, :video_file, :genre_name, :click_count, :is_liked
+  attributes :id, :slug, :name, :title, :description, :festival_laureates, :directed_by, :language, :released_date, :video_duration, :video_file, :trailer_file, :genre_name, :click_count, :is_liked
   has_one :genre, serializer: Api::V1::GenreSerializer
   has_one :movie_thumbnail, serializer: Api::V1::MovieThumbnailSerializer
-  has_many :user_video_last_stops
+  has_many :user_video_last_stops, serializer: Api::V1::UserVideoLastStopSerializer
   has_many :movie_captions, serializer: Api::V1::MovieCaptionSerializer
 
   def video_file
-    if (scope[:current_user] && scope[:current_user].is_payment_verified?)
-      object.hls_movie_url
-    end
+    object.hls_movie_url if valid_user?
   end
 
   def genre_name
@@ -16,11 +14,7 @@ class Api::V1::MovieSerializer < ActiveModel::Serializer
   end
 
   def user_video_last_stops
-    if scope[:current_user]
-      user_video_last_stop = scope[:current_user].user_video_last_stops.find_by(admin_movie_id: object.id)
-      ActiveModelSerializers::SerializableResource.new(user_video_last_stop,
-          serializer: Api::V1::UserVideoLastStopSerializer).serializable_hash if user_video_last_stop.present?
-    end
+    scope[:current_user].user_video_last_stops.find_by(admin_movie_id: object.id) if scope[:current_user]
   end
 
   def movie_captions
@@ -36,7 +30,15 @@ class Api::V1::MovieSerializer < ActiveModel::Serializer
     scope[:current_user].my_list_movie_ids.include?(object.id) if scope[:current_user]
   end
 
+  def trailer_file
+    #T0DO :need to implement trailer functionality, from admin side
+  end
+
   private
+
+  def valid_user?
+    (scope[:current_user] && scope[:current_user].is_payment_verified?)
+  end
 
   def bitly_response_parse_to_count_click (bitly_clicks_result)
     count_hash = {}
@@ -48,6 +50,6 @@ class Api::V1::MovieSerializer < ActiveModel::Serializer
 
   def get_click_count(results, domain)
     result = results.select {|result| result["domain"].include?(domain)}.first
-    result['clicks']
+    result['clicks'] if result.present?
   end
 end
