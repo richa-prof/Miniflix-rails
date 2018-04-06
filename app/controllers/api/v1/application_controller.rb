ae453660d070c9de75547050a3ba4f1cdda22c25
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-06 20:46:58 +0530

    X-App token added

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery prepend: true
+  before_action :valid_client_request?
 
   rescue_from 'ActiveRecord::RecordNotFound' do |exception|
     render json: {success: false, error_message: exception.message}
+  end
+
+  def valid_client_request?
+    unless request.headers['HTTP-X-CLIENT-TOKEN'] ==  ENV['HTTP_X_CLIENT_TOKEN']
+      render json: {message: 'You are not authorize to access this request'}, status: :forbidden and return
+    end
   end
 
 end
