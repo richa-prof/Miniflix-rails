0ba318e928d5dd674c99c7917d7432d05775e614
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-10 20:13:05 +0530

    Added react side pages urls in footer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
+
+  STATIC_PAGES_URLS_MAP = {
+      'about_us' => "#{ENV['Host']}/about_us",
+      'help_center' => "#{ENV['Host']}/help_center",
+      'term_of_services' => "#{ENV['Host']}/term_of_services",
+      'privacy_policy' => "#{ENV['Host']}/privacy_policy"
+    }.freeze
+
   def errors_for(form, field)
     content_tag(:span, form.object.errors[field].join(', '), class: 'blg-advice text-danger') unless form.object.errors[field].blank?
+  end
+
+  def get_static_page_url_for(target_uri)
+    STATIC_PAGES_URLS_MAP[target_uri]
   end
 end
