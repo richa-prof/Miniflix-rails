c3ee7a00b0c6e6e775fd0df9cf7c2689cb9dcaed
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-11 14:13:29 +0530

    add constraint for staging env

--- lib/constraints/blog_subdomain_constraint.rb
+++ lib/constraints/blog_subdomain_constraint.rb
@@ -5,10 +5,12 @@ module Constraints
       environment = Rails.env
 
       case environment
-        when 'production'
-          request.subdomain.present? && request.subdomain.start_with?('blog')
-        when 'development'
-          true
+      when 'production'
+        request.subdomain.present? && request.subdomain.start_with?('blog')
+      when 'staging'
+        request.subdomain.present? && request.subdomain.start_with?('stag-blog')
+      when 'development'
+        true
       end
     end
   end
