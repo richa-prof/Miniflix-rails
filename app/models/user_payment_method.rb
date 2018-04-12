9c1eaa7522a79bb22b4aaaf3c3429caccbcfe229
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-12 17:45:55 +0530

    minor changes

--- app/models/user_payment_method.rb
+++ app/models/user_payment_method.rb
@@ -1,7 +1,7 @@
 class UserPaymentMethod < ApplicationRecord
-  attr_encrypted :card_number, key: ENV['credit_card_number_key']
-  attr_encrypted :card_CVC , key: ENV['credit_card_cvc_key']
-  attr_encrypted :expiration_month, :expiration_year, key: ENV['month_year_key']
+  # attr_encrypted :card_number, key: ENV['credit_card_number_key']
+  # attr_encrypted :card_CVC , key: ENV['credit_card_cvc_key']
+  # attr_encrypted :expiration_month, :expiration_year, key: ENV['month_year_key']
 
   belongs_to :user
   has_many :user_payment_transactions, dependent: :destroy
