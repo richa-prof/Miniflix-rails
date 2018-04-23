2d3ebf0633cefadb95c44166ebc67c4658caae23
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-23 17:32:37 +0530

    user json response Changes   + current_payment_method attribute to show user payment method type

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,7 +1,12 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :registration_plan, :phone_number, :customer_id, :subscription_id, :cancelation_date, :receipt_data, :subscription_plan_status, :image, :sign_up_from, :cancelation_date, :payment_verified, :migrate_user, :valid_for_thankyou_page
+  attributes :id, :name, :email, :registration_plan, :phone_number, :customer_id, :subscription_id, :cancelation_date, :receipt_data, :subscription_plan_status, :image, :sign_up_from, :cancelation_date, :payment_verified, :migrate_user, :valid_for_thankyou_page, :current_payment_method
 
   def payment_verified
     object.is_payment_verified?
+  end
+
+  def current_payment_method
+    user_payment_methods = object.user_payment_methods
+    user_payment_methods.active.last.payment_type if user_payment_methods.present?
   end
 end
