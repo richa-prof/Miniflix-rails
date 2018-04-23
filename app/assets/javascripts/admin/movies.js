ed158bea6bd489df4b87926079f6bb8f8bdb3c03
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-23 16:35:13 +0530

    Changes on Admin Side Movies Show Page.   + Enabled jwplayer_setup on movies show page.   + Fixed the jwplayer JS not loading issue.

--- app/assets/javascripts/admin/movies.js
+++ app/assets/javascripts/admin/movies.js
@@ -1,38 +1,67 @@
-function jwplayer_setup (hls_url, original_url, caption_data){
-  jwplayer("flim_video").setup({
-    sources: [{
-        file: hls_url
-      },{
-        file: original_url
-      }],
-    autostart: true,
-    allowscriptaccess: 'always',
-    allownetworking: 'all',
-    tracks: caption_data
-  });
-}
+// Reference: http://developmentmode.wordpress.com/2011/05/09/defining-custom-functions-on-jquery/
+(function ($) {
 
-$(document).on("click", ".full-screen-play", function() {
-  $(".player").removeClass('hide');
-  jwplayer("flim_video").setFullscreen(true);
-  jwplayer("flim_video").play(true);
-});
+  jwPlayerConfigContainer = function() {
+    return $('#jwPlayerConfigContainer');
+  };
 
+  jwplayer_setup = function() {
+    var jwPlayerConfig = jwPlayerConfigContainer();
+    var hlsUrl = jwPlayerConfig.data('hls-file');
+    var originalUrl = jwPlayerConfig.data('original-file');
 
-$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(el){
+    jwplayer("flim_video").setup({
+      sources: [{
+          file: hlsUrl
+        },{
+          file: originalUrl
+        }],
+      autostart: true,
+      allowscriptaccess: 'always',
+      allownetworking: 'all'
+    });
+  };
 
-  var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement;
-  if (state != true){
-    jwplayer("flim_video").pause(true);
-    $(".player").addClass('hide');
-  } else{
-     jwplayer("flim_video").play(true);
-  }
-});
+  jwplayer_setup_error = function() {
+    jwplayer().on('setupError',function(er){
+      $('.full-screen-play').removeClass('full-screen-play');
 
-function jwplayer_setup_error(){
-  jwplayer().on('setupError',function(er){
-    $('.full-screen-play').removeClass('full-screen-play');
-    $('#err-player').html('<div class="alert alert-danger alert-dismissible" style="margin-top: 12px;"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+ er.message +'</div>');
-  });
-}
+      $('#err-player').html('<div class="alert alert-danger alert-dismissible" style="margin-top: 12px;"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'+ er.message +'</div>');
+    });
+  };
+
+  bindClickEventOnFullScreenPlayBtn = function() {
+    $('.full-screen-play').off('click').on('click', function(event) {
+      $(".player").removeClass('hide');
+      jwplayer("flim_video").setFullscreen(true);
+      jwplayer("flim_video").play(true);
+    })
+  };
+
+  bindChangeEventOnBrowserScreens = function() {
+    $(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(el){
+
+      var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement;
+      if (state != true){
+        jwplayer("flim_video").pause(true);
+        $(".player").addClass('hide');
+      } else{
+        jwplayer("flim_video").play(true);
+      }
+    });
+  };
+
+}) (jQuery);
+
+var ready;
+
+ready = function() {
+  jwplayer.key = jwPlayerConfigContainer().data('jwplayer-key');
+  bindClickEventOnFullScreenPlayBtn();
+  bindChangeEventOnBrowserScreens();
+  jwplayer_setup();
+  jwplayer_setup_error();
+};
+
+$(document).ready(ready);
+$(document).on('turbolinks:load', ready);
