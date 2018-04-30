deabaa585e1d0947c8b7832df72f0863987e0f27
forever-sumit <forever.sumitsharma@gmail.com>
2018-04-30 18:58:36 +0530

    Handled strip webhook for payment failed event.

--- app/controllers/api/v1/stripe_payments_controller.rb
+++ app/controllers/api/v1/stripe_payments_controller.rb
@@ -2,26 +2,37 @@ class Api::V1::StripePaymentsController < ApplicationController
   before_action :set_user
 
   def hook
-    if params[:type] == "invoice.payment_succeeded"
+    event_type = params[:type]
+    if event_type == "invoice.payment_succeeded"
       response = Stripe::TransactionSave.new(@user, params[:stripe_payment]).call
-    else
-      #TODO: need to implement payment fail scenario need to expire user subscription status
+    elsif event_type == 'invoice.payment_failed' && !@user.expired?
+      # Set user's `subscription_plan_status` to `Expired`.
+      response = if @user.expired!
+                   { sucess: true, message: 'successfully detail save' }
+                 else
+                   { sucess: false, message: @user.errors.full_messages }
+                 end
     end
+
     render json: response
   end
 
   private
   def set_user
     @user = User.find_by_customer_id_and_subscription_id(customer_id, subscription_id)
-    puts "user not present" unless @user
-    render json: {message: "user not found"} and return unless @user
+    puts "user not present with customer_id: #{customer_id} and subscription_id: #{subscription_id}" unless @user
+    render json: { message: "user not found" } and return unless @user
+  end
+
+  def payment_data_object
+    params[:stripe_payment][:data][:object]
   end
 
   def customer_id
-    params[:stripe_payment][:data][:object][:customer]
+    payment_data_object[:customer]
   end
 
   def subscription_id
-    params[:stripe_payment][:data][:object][:subscription]
+    payment_data_object[:subscription]
   end
 end
