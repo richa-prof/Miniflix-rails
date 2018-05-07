8233e5d5e64765a2a5955427e79eeab25db72a6f
forever-sumit <forever.sumitsharma@gmail.com>
2018-05-07 14:58:15 +0530

    Done with `users/payment_history` API.

--- app/models/user_payment_transaction.rb
+++ app/models/user_payment_transaction.rb
@@ -5,4 +5,14 @@ class UserPaymentTransaction < ApplicationRecord
   validates_uniqueness_of :transaction_id, allow_blank: true, allow_nil: true
 
   delegate :payment_type, to: :user_payment_method, prefix: :transcation,  allow_nil: true
+
+  # ======= Related to mobile API's start =======
+  def payment_type
+    self.transcation_payment_type
+  end
+
+  def as_json(options=nil)
+    super(except: [:created_at, :updated_at, :user_payment_method_id])
+  end
+  # ======= Related to mobile API's start =======
 end
