fde8eae2a781e5e0304b2a0cb6147a928b50231c
forever-sumit <forever.sumitsharma@gmail.com>
2018-05-08 20:27:12 +0530

    Display JSON response on rails root page.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Sidekiq::Web, at: '/sidekiq'
 
+  get '/' => 'welcome#index'
+
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', controllers: {
     sessions: "api/v1/sessions",
     registrations: "api/v1/registrations",
