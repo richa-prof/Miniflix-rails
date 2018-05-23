b5e6d73c800dc56c811aaf05c23eca4be55e3bc8
forever-sumit <forever.sumitsharma@gmail.com>
2018-05-23 18:43:20 +0530

    Set `config.sandbox` paypal configuration to false for production.

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,7 +1,7 @@
 require "paypal/recurring"
 
 PayPal::Recurring.configure do |config|
-  config.sandbox = true
+  config.sandbox = Rails.env.production? ? false : true
   config.username = ENV['Paypal_Username']
   config.password = ENV['Paypal_Password']
   config.signature = ENV['Paypal_signature']
