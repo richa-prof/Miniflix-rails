d15297752d89488ea4043e6ee10be78af31b559c
forever-sumit <forever.sumitsharma@gmail.com>
2018-05-28 15:47:59 +0530

    Send reset password reminder message if Rails env is `production`.

--- app/jobs/send_reset_password_reminder_job.rb
+++ app/jobs/send_reset_password_reminder_job.rb
@@ -2,16 +2,18 @@ class SendResetPasswordReminderJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    user = User.find(user_id)
+    if Rails.env.production?
+      user = User.find(user_id)
 
-    if user.phone_number.present?
-      user.send_reset_password_reminder_sms
-    end
+      if user.phone_number.present?
+        user.send_reset_password_reminder_sms
+      end
 
-    if user.email? && user.Web?
-      user.send_reset_password_reminder_email
-    elsif user.iOS? || user.Android?
-      user.send_reset_password_reminder_push_notification
+      if user.email? && user.Web?
+        user.send_reset_password_reminder_email
+      elsif user.iOS? || user.Android?
+        user.send_reset_password_reminder_push_notification
+      end
     end
   end
 end
