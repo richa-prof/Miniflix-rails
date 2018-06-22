2381d7d4c2c32c7332f22778ece86c6c9f7f5de1
forever-sumit <forever.sumitsharma@gmail.com>
2018-06-22 15:18:08 +0530

    Changed `browser_title` to `title` in `SeoMetaSerializer`.

--- app/serializers/api/v1/seo_meta_serializer.rb
+++ app/serializers/api/v1/seo_meta_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V1::SeoMetaSerializer < ActiveModel::Serializer
-  attributes :id, :browser_title, :keywords, :description
+  attributes :id, :title, :keywords, :description
+
+  def title
+    object.browser_title
+  end
 
   def keywords
     object.meta_keywords
