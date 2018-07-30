698d7afe04f9ba07aa56d21a81de46d27b7bbf3e
forever-sumit <forever.sumitsharma@gmail.com>
2018-07-30 19:49:56 +0530

    Save the background images to s3 + used cloudfront url to fetch.

--- app/models/background_image.rb
+++ app/models/background_image.rb
@@ -3,4 +3,10 @@ class BackgroundImage < ApplicationRecord
 
   # VALIDATIONS
   validates_presence_of :image_file
+
+  def image_file_cloud_front_url
+    target_path = image_file.try(:path)
+
+    CommonHelpers.cloud_front_url(target_path)
+  end
 end
