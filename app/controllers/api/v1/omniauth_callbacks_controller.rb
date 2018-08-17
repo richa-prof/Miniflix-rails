c59b9629f3cc1180484d0d50fcbaf1dd9139a483
forever-sumit <forever.sumitsharma@gmail.com>
2018-08-17 16:19:11 +0530

    Fixed the issue `ActiveRecord::RecordNotUnique occurred in omniauth_callbacks#omniauth_success`.

--- app/controllers/api/v1/omniauth_callbacks_controller.rb
+++ app/controllers/api/v1/omniauth_callbacks_controller.rb
@@ -1,13 +1,31 @@
 class Api::V1::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
   include Api::V1::Concerns::UserSerializeDataConcern
-  def omniauth_success
-    super do
-      response.headers['client'] = @auth_params[:client_id]
-      response.headers['access-token'] =  @auth_params[:auth_token]
-      response.headers['uid'] = @auth_params[:uid]
-      response.headers['expiry'] =  @auth_params[:expiry]
 
-      redirect_to react_redirect_url and return
+  def omniauth_success
+    db_user = User.find_by_email(auth_hash['info']['email'])
+    can_proceed = true
+
+    if db_user.present?
+      old_provider = db_user.provider
+      new_provider = auth_hash['provider']
+      can_proceed = old_provider == new_provider
+    end
+
+    if can_proceed
+      super do
+        response.headers['client'] = auth_hash[:client_id]
+        response.headers['access-token'] =  @auth_params[:auth_token]
+        response.headers['uid'] = @auth_params[:uid]
+        response.headers['expiry'] =  @auth_params[:expiry]
+
+        redirect_to react_redirect_url and return
+      end
+    else
+      error_message = I18n.t('flash.omniauth.signup_fail', provider: old_provider)
+
+      target_url = "#{ENV['Host']}/sign_in?success=false&&message=#{error_message}"
+
+      redirect_to target_url and return
     end
   end
 
