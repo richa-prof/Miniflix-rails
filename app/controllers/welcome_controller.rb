b77a09d154f14b620a812d7389122bd9d1646c42
forever-sumit <forever.sumitsharma@gmail.com>
2018-10-13 15:55:09 +0530

    remove previous mobile api doc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,4 @@ class WelcomeController < ApplicationController
   def index
     render json: { success: true }
   end
-
-  def api_help
-    @target_host = "#{ENV['RAILS_API_HOST']}/api/vm1/"
-  end
 end
