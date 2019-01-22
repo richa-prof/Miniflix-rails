311543232c2f00a8c239e086c5c6d2bad839c6c1
Oleg <belinskiy21@gmail.com>
2019-01-22 21:40:12 +0200

    add aplication.yml to watch list spring

--- app/utils/common_helpers.rb
+++ app/utils/common_helpers.rb
@@ -3,6 +3,6 @@ module CommonHelpers
   module_function
 
   def cloud_front_url(path)
-    'https://d32c9chnc7rnl8.cloudfront.net/' + path if path.present?
+    ENV['CLOUD_FRONT_URL'] + path if path.present?
   end
 end
