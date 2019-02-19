2c01c2ab3ba8a45d9255b3b0efe6990961edd394
Oleg <belinskiy21@gmail.com>
2019-02-19 20:49:47 +0100

    add path

--- app/utils/common_helpers.rb
+++ app/utils/common_helpers.rb
@@ -3,6 +3,6 @@ module CommonHelpers
   module_function
 
   def cloud_front_url(path)
-    ENV['CLOUD_FRONT_URL'] + path.path if path.present?
+    ENV['CLOUD_FRONT_URL'] + path if path.present?
   end
 end
