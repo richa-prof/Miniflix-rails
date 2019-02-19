951824c6e98e98cb4b33431a1c8f917d362d19de
Oleg <belinskiy21@gmail.com>
2019-02-19 20:28:42 +0100

    s to mount

--- app/models/serial_thumbnail.rb
+++ app/models/serial_thumbnail.rb
@@ -2,10 +2,10 @@ class SerialThumbnail < ApplicationRecord
   self.table_name = "admin_serial_thumbnails"
   belongs_to :serial, :foreign_key => 'admin_serial_id'
 
-  mount_uploader :serial_screenshot_1, SerialThumbnailUploader
-	mount_uploader :serial_screenshot_2, SerialThumbnailUploader
-	mount_uploader :serial_screenshot_3, SerialThumbnailUploader
-	mount_uploader :thumbnail_screenshot, SerialThumbnailUploader
-	mount_uploader :thumbnail_640_screenshot, SerialThumbnailUploader
-  mount_uploader :thumbnail_800_screenshot, SerialThumbnailUploader
+  mount_uploaders :serial_screenshot_1, SerialThumbnailUploader
+	mount_uploaders :serial_screenshot_2, SerialThumbnailUploader
+	mount_uploaders :serial_screenshot_3, SerialThumbnailUploader
+	mount_uploaders :thumbnail_screenshot, SerialThumbnailUploader
+	mount_uploaders :thumbnail_640_screenshot, SerialThumbnailUploader
+  mount_uploaders :thumbnail_800_screenshot, SerialThumbnailUploader
 end
