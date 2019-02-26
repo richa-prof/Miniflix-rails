24d7ea5b0867c587403b39904f3bf9d2b0a86deb
PC <sasha.holyk@gmail.com>
2019-02-26 16:24:11 +0200

    Add validation to add series page and max-width to series table

--- app/assets/javascripts/form_helpers.js
+++ app/assets/javascripts/form_helpers.js
@@ -1,30 +1,37 @@
 // Reference: http://developmentmode.wordpress.com/2011/05/09/defining-custom-functions-on-jquery/
-(function ($) {
-
+(function($) {
   fileFields = function() {
     return $(".uploadfile-field .form-control");
   };
 
   bindChangeEventOnFileFields = function() {
-    fileFields().change(function(){
-      var fileFieldVal = $(this).val().replace(/C:\\fakepath\\/i, '');
-      var targetField = $(this).closest(".upload-file-all").find(".targetFileField");
+    fileFields().change(function() {
+      var fileFieldVal = $(this)
+        .val()
+        .replace(/C:\\fakepath\\/i, "");
+      var targetField = $(this)
+        .closest(".upload-file-all")
+        .find(".targetFileField");
       targetField.val(fileFieldVal);
-    })
+    });
   };
 
   bindChangeEventOnMovieThumbnailFileFields = function() {
-    $('.movie-thumbnail-file-field').off('change').on('change', function(event) {
-      var fileFieldId = $(this).attr('id');
-      validFile(fileFieldId);
-    })
+    $(".movie-thumbnail-file-field")
+      .off("change")
+      .on("change", function(event) {
+        var fileFieldId = $(this).attr("id");
+        validFile(fileFieldId);
+      });
   };
 
   bindClickEventOnMovieThumbnailFileFieldWrappers = function() {
-    $('.movie-thumbnail-file-field-wrapper').off('click').on('click', function(event) {
-      var targetObj = $(this).siblings('.movie-thumbnail-file-field');
-      targetObj.click();
-    })
+    $(".movie-thumbnail-file-field-wrapper")
+      .off("click")
+      .on("click", function(event) {
+        var targetObj = $(this).siblings(".movie-thumbnail-file-field");
+        targetObj.click();
+      });
   };
 
   adminStaffMemberForm = function() {
@@ -32,16 +39,18 @@
   };
 
   adminSendReplyToVisitorForm = function() {
-    return $('#frm_send_reply');
+    return $("#frm_send_reply");
   };
 
   applyValidationToStaffMemberForm = function() {
     var formObject = adminStaffMemberForm();
     formObject.validate({
       rules: {
-        "user[email]": { required: true,
-                         email: true,
-                         remote: formObject.data('check-email-path') }
+        "user[email]": {
+          required: true,
+          email: true,
+          remote: formObject.data("check-email-path")
+        }
       },
       messages: {
         "user[name]": {
@@ -60,9 +69,8 @@
       ignore: [],
       rules: {
         "contact_user_reply[message]": {
-           required: function()
-          {
-           CKEDITOR.instances.contact_user_reply_message.updateElement();
+          required: function() {
+            CKEDITOR.instances.contact_user_reply_message.updateElement();
           }
         }
       },
@@ -84,8 +92,10 @@
 
     formObject.validate({
       rules: {
-        "genre[name]": { required: true,
-                         remote: formObject.data('check-name-url') },
+        "genre[name]": {
+          required: true,
+          remote: formObject.data("check-name-url")
+        },
         "genre[color]": { required: true }
       },
 
@@ -95,14 +105,14 @@
           remote: "Genre name already exist."
         },
         "genre[color]": {
-          required: "Please enter a color",
+          required: "Please enter a color"
         }
       }
     });
   };
 
   adminBackgroundImageForm = function() {
-    return $('#new_background_image');
+    return $("#new_background_image");
   };
 
   applyValidationToAdminBackgroundImageForm = function() {
@@ -121,69 +131,75 @@
   };
 
   adminAddMovieDetailsForm = function() {
-    return $('#frm_admin_movie.add_movie_details_frm');
+    return $("#frm_admin_movie.add_movie_details_frm");
   };
 
   adminMovieEditForm = function() {
-    return $('#frm_admin_movie.edit_movie_frm');
+    return $("#frm_admin_movie.edit_movie_frm");
   };
 
   adminMovieFormValidationRules = function() {
     var rulesMap = {
-          "movie[name]": {required: true},
-          "movie[title]": {required: true},
-          "movie[description]": {required: true},
-          "movie[admin_genre_id]": {required: true},
-          "movie[video_type]": {required: true},
-          "movie[video_size]": {required: true},
-          "movie[video_duration]": {required: true},
-          "movie[video_format]": {required: true},
-          "movie[directed_by]": {required: true},
-          "movie[released_date]": {required: true},
-          "movie[language]": {required: true},
-          "movie[posted_date]": {required: true},
-          "movie[star_cast]": {required: true},
-          "movie[actors]": {required: true}
-        };
+      "movie[name]": { required: true },
+      "movie[title]": { required: true },
+      "movie[description]": { required: true },
+      "movie[admin_genre_id]": { required: true },
+      "movie[video_type]": { required: true },
+      "movie[video_size]": { required: true },
+      "movie[video_duration]": { required: true },
+      "movie[video_format]": { required: true },
+      "movie[directed_by]": { required: true },
+      "movie[released_date]": { required: true },
+      "movie[language]": { required: true },
+      "movie[posted_date]": { required: true },
+      "movie[star_cast]": { required: true },
+      "movie[actors]": { required: true }
+    };
 
     return rulesMap;
   };
 
   adminMovieFormValidationMessages = function() {
     var messagesMap = {
-          "movie[name]": {required: "Please enter a movie name."},
-          "movie[title]": {required: "Please enter a movie title."},
-          "movie[description]": {required: "Please enter a movie description."},
-          "movie[admin_genre_id]": {required: "Please select genre type."},
-          "movie[video_type]": {required: "Please enter a video type."},
-          "movie[video_size]": {required: "Please enter a video size."},
-          "movie[video_duration]": {required: "Please enter a video duration."},
-          "movie[video_format]": {required: "Please enter a video format."},
-          "movie[directed_by]": {required: "Please enter a directer name."},
-          "movie[released_date]": {required: "Please select a released date."},
-          "movie[language]": {required: "Please enter a language."},
-          "movie[posted_date]": {required: "Please select a posted date."},
-          "movie[star_cast]": {required: "Please enter a star cast."},
-          "movie[actors]": {required: "Please enter an actor."}
-        };
+      "movie[name]": { required: "Please enter a movie name." },
+      "movie[title]": { required: "Please enter a movie title." },
+      "movie[description]": { required: "Please enter a movie description." },
+      "movie[admin_genre_id]": { required: "Please select genre type." },
+      "movie[video_type]": { required: "Please enter a video type." },
+      "movie[video_size]": { required: "Please enter a video size." },
+      "movie[video_duration]": { required: "Please enter a video duration." },
+      "movie[video_format]": { required: "Please enter a video format." },
+      "movie[directed_by]": { required: "Please enter a directer name." },
+      "movie[released_date]": { required: "Please select a released date." },
+      "movie[language]": { required: "Please enter a language." },
+      "movie[posted_date]": { required: "Please select a posted date." },
+      "movie[star_cast]": { required: "Please enter a star cast." },
+      "movie[actors]": { required: "Please enter an actor." }
+    };
     return messagesMap;
   };
 
   adminMovieThumbnailValidationRules = function() {
     var rulesMap = {
-          "movie_thumbnail[movie_screenshot_1]": {required: true},
-          "movie_thumbnail[movie_screenshot_2]": {required: true},
-          "movie_thumbnail[movie_screenshot_3]": {required: true}
-        };
+      "movie_thumbnail[movie_screenshot_1]": { required: true },
+      "movie_thumbnail[movie_screenshot_2]": { required: true },
+      "movie_thumbnail[movie_screenshot_3]": { required: true }
+    };
     return rulesMap;
   };
 
   adminMovieThumbnailValidationMessages = function() {
     var messagesMap = {
-          "movie_thumbnail[movie_screenshot_1]": {required: "Please upload movie thumbnail."},
-          "movie_thumbnail[movie_screenshot_2]": {required: "Please upload movie thumbnail."},
-          "movie_thumbnail[movie_screenshot_3]": {required: "Please upload movie thumbnail."}
-        };
+      "movie_thumbnail[movie_screenshot_1]": {
+        required: "Please upload movie thumbnail."
+      },
+      "movie_thumbnail[movie_screenshot_2]": {
+        required: "Please upload movie thumbnail."
+      },
+      "movie_thumbnail[movie_screenshot_3]": {
+        required: "Please upload movie thumbnail."
+      }
+    };
 
     return messagesMap;
   };
@@ -191,13 +207,13 @@
   adminMovieAndThumbnailValidationRulesMap = function() {
     var rules1 = adminMovieFormValidationRules();
     var rules2 = adminMovieThumbnailValidationRules();
-    return $.extend( rules1, rules2 );
+    return $.extend(rules1, rules2);
   };
 
   adminMovieAndThumbnailValidationMessagesMap = function() {
     var messages1 = adminMovieFormValidationMessages();
     var messages2 = adminMovieThumbnailValidationMessages();
-    return $.extend( messages1, messages2 );
+    return $.extend(messages1, messages2);
   };
 
   applyValidationToAdminMovieForm = function() {
@@ -229,45 +245,57 @@
   };
 
   open_file_upload = function(fileId) {
-    $("#"+fileId).click();
+    $("#" + fileId).click();
   };
 
   validFile = function(fileId) {
-    $("#"+fileId).valid();
+    $("#" + fileId).valid();
   };
 
   enableDatePicker = function() {
-    var dateInputFieldObjects = $('.date-picker');
+    var dateInputFieldObjects = $(".date-picker");
 
     if (dateInputFieldObjects.length > 0) {
       dateInputFieldObjects.each(function() {
         $(this).datepicker({
-          autoclose: true
+          autoclose: true,
+          startDate: "01.01.0001"
         });
-      })
+      });
     }
   };
 
   enableICheckToCheckboxes = function() {
-    var minimalCheckboxObjects = $('input[type="checkbox"].minimal, input[type="radio"].minimal');
+    var minimalCheckboxObjects = $(
+      'input[type="checkbox"].minimal, input[type="radio"].minimal'
+    );
 
     if (minimalCheckboxObjects.length > 0) {
       minimalCheckboxObjects.each(function() {
         $(this).iCheck({
-          checkboxClass: 'icheckbox_minimal-blue',
-          radioClass: 'iradio_minimal-blue'
+          checkboxClass: "icheckbox_minimal-blue",
+          radioClass: "iradio_minimal-blue"
         });
-      })
+      });
     }
   };
 
   enableMaskToVideoDurationField = function() {
-    if ($('#movie_video_duration').length) {
-      $('#movie_video_duration').mask('00:00:00', { placeholder: "00:00:00" });
+    if ($("#movie_video_duration").length) {
+      $("#movie_video_duration").mask("00:00:00", { placeholder: "00:00:00" });
     }
   };
 
-}) (jQuery);
+  bindKeypressEventOnSeasonsNumberField = function(e) {
+    $("#serial_seasons_number").on("keypress", function(e) {
+      const iKeyCode = e.which ? e.which : e.keyCode;
+      if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
+        return false;
+
+      return true;
+    });
+  };
+})(jQuery);
 
 var ready;
 
@@ -277,24 +305,25 @@ ready = function() {
   enableMaskToVideoDurationField();
   bindChangeEventOnMovieThumbnailFileFields();
   bindClickEventOnMovieThumbnailFileFieldWrappers();
+  bindKeypressEventOnSeasonsNumberField();
 
   if (fileFields().length) {
     bindChangeEventOnFileFields();
   }
 
-  if ( adminStaffMemberForm().length ) {
+  if (adminStaffMemberForm().length) {
     applyValidationToStaffMemberForm();
   }
 
-  if ( adminSendReplyToVisitorForm().length ) {
+  if (adminSendReplyToVisitorForm().length) {
     applyValidationToContactUserReplyForm();
   }
 
-  if ( adminGenreForm().length ) {
+  if (adminGenreForm().length) {
     applyValidationToAdminGenreForm();
   }
 
-  if ( adminBackgroundImageForm().length ) {
+  if (adminBackgroundImageForm().length) {
     applyValidationToAdminBackgroundImageForm();
   }
 
@@ -308,4 +337,4 @@ ready = function() {
 };
 
 $(document).ready(ready);
-$(document).on('turbolinks:load', ready);
+$(document).on("turbolinks:load", ready);
