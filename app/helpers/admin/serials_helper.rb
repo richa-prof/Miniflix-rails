fdc89b478bd603070a69d76a045577ef389db0c8
Serhii Kudryk <sergiy.kudryk@gmail.com>
2019-02-27 18:18:40 +0200

    small fixes and unification for thumbnail display code

--- app/helpers/admin/serials_helper.rb
+++ app/helpers/admin/serials_helper.rb
@@ -1,9 +1,9 @@
 module Admin::SerialsHelper
 
-  def formatted_released_date_for(serial)
-    released_date = serial.year
+  def formatted_released_date_for_serial(serial)
+    released_date = serial&.year
 
-    return t('label.not_available') unless released_date.present?
+    return t('label.not_available') unless released_date&.present?
 
     released_date.to_s(:full_date_month_and_year_format)
   end
@@ -18,7 +18,7 @@ module Admin::SerialsHelper
     image_tag(target_image).html_safe
   end
 
-  def get_default_thumbnail_url_for(thumbnail_type)
+  def get_default_thumbnail_url_for_serial(thumbnail_type)
     case thumbnail_type
     when 'thumbnail_640_screenshot'
       'admin/upload_img640.jpg'
