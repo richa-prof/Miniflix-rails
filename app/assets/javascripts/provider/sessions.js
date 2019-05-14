133ed32cc57c6bcdaaa1b752c66b316ead3fd048
Serhii Kudryk <sergiy.kudryk@gmail.com>
2019-05-14 22:36:56 +0300

    password preview logic for sign in dialog

--- app/assets/javascripts/provider/sessions.js
+++ app/assets/javascripts/provider/sessions.js
@@ -13,4 +13,22 @@ $(document).on('ready turbolinks:load', function() {
       }
     }
   });
+
+  $('.password-icon').on('click', function(ev) {
+    if (window.lock) { return false;}
+    window.lock = true;
+    let el = $('.password-icon');
+    let inp = el.parent().find('.password-input');
+    let tt = inp.attr('type');
+    if (tt == 'password') {
+      el.find('.js-eye-invisible').addClass('hidden');
+      el.find('.js-eye-visible').removeClass('hidden');
+      inp.attr('type', 'text');
+    } else {
+      el.find('.js-eye-visible').addClass('hidden');
+      el.find('.js-eye-invisible').removeClass('hidden');
+      inp.attr('type', 'password');
+    }
+    setTimeout(function(){window.lock = false;}, 100);
+  });
 });
