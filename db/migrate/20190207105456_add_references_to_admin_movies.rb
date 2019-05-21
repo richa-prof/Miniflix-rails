3fed8689cdd022bf8e0ccb9cbf386e2f92b7e6c0
Serhii Kudryk <sergiy.kudryk@gmail.com>
2019-05-21 16:11:03 +0300

    fix migration; enable support for ES6 syntax for uglifier in production env.

--- db/migrate/20190207105456_add_references_to_admin_movies.rb
+++ db/migrate/20190207105456_add_references_to_admin_movies.rb
@@ -1,5 +1,5 @@
 class AddReferencesToAdminMovies < ActiveRecord::Migration[5.1]
   def change
-    add_column :admin_movies, :season, foreign_key: true, type: :integer
+    add_column :admin_movies, :season_id, :integer, foreign_key: true
   end
 end
