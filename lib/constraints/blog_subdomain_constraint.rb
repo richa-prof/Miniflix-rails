a5e5c1b2e29ae10c5f12dd23ec8e9b5611651874
Dharin Rajgor <dharin.rajgor@gmail.com>
2019-07-29 12:20:20 +0530

    qa routes contraints

--- lib/constraints/blog_subdomain_constraint.rb
+++ lib/constraints/blog_subdomain_constraint.rb
@@ -9,6 +9,8 @@ module Constraints
         request.subdomain.present? && request.subdomain.start_with?('blog')
       when 'staging'
         request.subdomain.present? && request.subdomain.start_with?('stag-blog')
+      when 'qa'
+        request.subdomain.present? && request.subdomain.start_with?('qa-blog')
       when 'development'
         true
       end
