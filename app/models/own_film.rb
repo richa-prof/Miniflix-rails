722e390e2eb59fde8591873e635ef1d00363a1e1
Dharin Rajgor <dharin.rajgor@gmail.com>
2019-08-02 10:54:40 +0530

    solve content movie bugs

--- app/models/own_film.rb
+++ app/models/own_film.rb
@@ -3,6 +3,7 @@ class OwnFilm < ApplicationRecord
   belongs_to :user
   before_save :check_same_film_id_and_type_exist
 
+  private
   def check_same_film_id_and_type_exist
     old_data = OwnFilm.find_by(user_id: self.user_id, film_id: self.film_id)
     old_data.destroy if old_data.present?
