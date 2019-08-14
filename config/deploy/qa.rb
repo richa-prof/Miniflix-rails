867e41fddf06c155ad6a3eadf820bd0906d1d147
Dharin Rajgor <dharin.rajgor@gmail.com>
2019-08-14 19:32:48 +0530

    ip chnaged for qa

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.43.36.175', user: 'ubuntu', roles: %w{app db web}
+server '54.191.244.16', user: 'ubuntu', roles: %w{app db web}
 set :deploy_to, "/var/www/qa/miniflix_rails"
 
 set :stage, "qa"
